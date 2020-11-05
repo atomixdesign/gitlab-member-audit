e9da6463ef22622f1d958d9d49268c4c102402f0
Jack Hillman <jack@atomix.com.au>
2020-11-06 09:04:37 +1030

    🚚 Moved panel-group to correct location

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useAppSelector } from './app/store'
 import { GroupList } from './components/group-list/group-list'
 import { MemberList } from './components/member-list/member-list'
-import { PanelGroup } from './panel-group/panel-group'
+import { PanelGroup } from './components/panel-group/panel-group'
 import { Panel } from './components/panel/panel'
 
 export const App = () => {
